cbd10877ca809d601e56a1703eac1f0fa128de56
abdelfattah <abdelfattah@gail.com>
2023-02-19 12:38:41 +0200

    my website

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var typed = new Typed('#span', {
+    strings: ["Front-End", "Back-End", "Full-Stack"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+var typed = new Typed('#nspan', {
+    strings: ["Front-End", "Back-End", "Full-Stack"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
